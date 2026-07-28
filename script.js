eee011b53f72164e3ca3bdf98c06f13baa2a9217
rey-1e <kanaderaj1216@gmail.com>
2026-07-28 18:58:12 +0530

    made arrow function for 'click' event listener better, retrieves amount entered by user(handles unwanted values) retrieves from and to currency uses FETCH API to make a API call to user's from Input value. succcessfully accesses the from Input value to toInput value

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 let convertBtn = document.querySelector("#convert-btn");
 let dropdownFrom = document.querySelector("#from-currency");
@@ -17,3 +17,41 @@ for(const currencyCode in countryList) {
 
 }
 
+convertBtn.addEventListener("click", async () => {
+    
+
+    //query selector for user's input field.
+    let userInput = document.querySelector(".form-group input");
+
+    //exact amount the user has entered.
+    let amount = userInput.value;
+
+    //check for edge cases.
+    if(amount <= 0) {
+        amount = 1; 
+        userInput.value.innerText = "1";
+    }
+
+    //FROM INPUT; 
+    let fromInput = dropdownFrom.value;
+
+    //TO INPUT; 
+    let toInput = dropdownTo.value;
+
+    //query selector for the output field; 
+    let outputPlace = document.querySelector("#result");
+
+    //call the fetch API; 
+    //wait first; 
+    let calculation = await fetch(`${BASE_URL}/${fromInput.toLowerCase()}.json`);
+    let usableObj = await calculation.json();
+
+    let toInputBetter = toInput.toLowerCase()
+    console.log(usableObj.usd[toInputBetter]);
+
+    
+
+
+
+    
+})
